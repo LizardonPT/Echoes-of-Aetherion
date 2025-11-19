e7250f9eda6b329967bfb503458c2f94fb81ab42
“[Breno <manancialgd@gmail.com>
2025-11-19 17:28:57 +0000

    Remove unused slowing radius

--- Assets/Scripts/Enemies/SteeringBehaviours/StopBehaviour.cs
+++ Assets/Scripts/Enemies/SteeringBehaviours/StopBehaviour.cs
@@ -4,8 +4,6 @@ namespace EchoesOfEtherion.Enemies.SteeringBehaviours
 {
     public class StopBehaviour : SteeringBehaviour
     {
-        [SerializeField] private float slowingRadius = 2f;
-
         public override Vector2 GetSteering(GameObject target)
         {
             if (Velocity.magnitude > 0.1f)
@@ -16,13 +14,5 @@ namespace EchoesOfEtherion.Enemies.SteeringBehaviours
 
             return Vector2.zero;
         }
-
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            //! This is important since we shouldn't divide by 0.
-            slowingRadius = slowingRadius <= 0 ? 1e-5f : slowingRadius;
-        }
-#endif
     }
 }
